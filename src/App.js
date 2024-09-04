08c718a48b3101c50c93ee4a8108aff1b4726d05
ilovezerocokeya <147008077a@gmail.com>
2024-09-04 16:30:59 +0900

    update: cityList css 추가

--- src/App.js
+++ src/App.js
@@ -21,6 +21,22 @@ export default function App($app) {
   const cityList = new CityList({
     $app,
     initialState: this.state.cities,
+    handleLoadMore: async () => {
+      const newStartIdx = this.state.startIdx + 40;
+      const newCities = await request(
+        newStartIdx,
+        this.state.region,
+        this.state.sortBy
+      );
+      this.setState({
+        ...this.state,
+        startIdx: newStartIdx,
+        cities: {
+          cities: [...this.state.cities.cities, ...newCities.cities],
+          isEnd: newCities.isEnd,
+        },
+      });
+    },
   });
   const cityDetail = new CityDetail();
 
