ec4857109db554c96bcf1483d1ffc442f111e7ee
ilovezerocokeya <147008077a@gmail.com>
2024-09-04 16:29:08 +0900

    feature: cityList작업

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+//Header.js
+export default function Header() {
+  this.$target = document.createElement("div");
+  this.$target.className = "header";
+
+  this.template = () => {};
+
+  this.render = () => {};
+
+  this.setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+
+  this.render();
+}
---
+++ src/components/RegionList.js
@@ -0,0 +1,16 @@
+//RegionList.js
+export default function RegionList() {
+  this.$target = document.createElement("div");
+  this.$target.className = "region-list";
+
+  this.template = () => {};
+
+  this.render = () => {};
+
+  this.setState = (newState) => {
+    this.state = newState;
+    this.render();
+  };
+
+  this.render();
+}
