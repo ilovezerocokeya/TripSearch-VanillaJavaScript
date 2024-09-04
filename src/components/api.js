00eb00ea8e1faeeb02c18fb307946fcdbf8935ab
ilovezerocokeya <147008077a@gmail.com>
2024-09-04 18:48:49 +0900

    feature: cityDetail 완성

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
-//api.js
 const API_URL = "https://trip-wiki-api.vercel.app/";
 
+//LIST API
 export const request = async (startIdx, region, sortBy, searchWord) => {
   try {
     let url = `${API_URL}`;
@@ -15,11 +15,26 @@ export const request = async (startIdx, region, sortBy, searchWord) => {
     if (searchWord) {
       url += `&search=${searchWord}`;
     }
-
+    console.log(url);
     //API 호출
     const response = await fetch(url);
     if (response) {
       let data = await response.json();
+      console.log(data);
+      return data;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//DETAIL API
+export const requestCityDetail = async (cityId) => {
+  try {
+    const response = await fetch(`${API_URL}city/${cityId}`);
+    if (response) {
+      let data = await response.json();
+      console.log(data);
       return data;
     }
   } catch (err) {
